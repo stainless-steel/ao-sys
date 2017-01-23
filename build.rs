4affe62692fd603996ceff712ec117195d1d7009
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-01-23 08:32:19 +0100

    Make a cosmetic adjustment

--- build.rs
+++ build.rs
@@ -19,18 +19,13 @@ fn main() {
     if pkg_config::find_library("ao").is_ok() {
         return;
     }
-
     let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("source");
     let output = PathBuf::from(&get!("OUT_DIR"));
-
     let build = output.join("build");
     ok!(fs::create_dir_all(&build));
-
     run!(cmd!(source.join("configure")).current_dir(&build)
                                        .arg(&format!("--prefix={}", output.display())));
-
     run!(cmd!("make").current_dir(&build).arg("install"));
-
     println!("cargo:rustc-link-lib=dylib=ao");
     println!("cargo:rustc-link-search={}", output.join("lib").display());
 }
